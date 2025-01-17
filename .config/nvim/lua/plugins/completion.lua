39fb8d73ff6c7f3f01d5d89322e815a8b2054920
hvoc981 <selbaha3@gmail.com>
2025-01-17 18:18:00 +0100

    borders removed from completion window

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -38,8 +38,8 @@ return {
 					end,
 				},
 				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
+					-- completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
 				},
 
 				mapping = cmp.mapping.preset.insert({
